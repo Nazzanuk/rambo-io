f930809eba5c13cd38a0126d056e9f90ff900d64
Nathan N <nazzanuk@gmail.com>
2015-02-26 15:59:05 +0000

    test change

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 $(boot2docker shellinit)
 docker build --tag rambo-io .
 docker rm rambo-io .
-docker run -p 5000:8082 --name="rambo-io" -d rambo-io
+docker run -p 80:8082 --name="rambo-io" -d rambo-io
