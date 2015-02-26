38f155add2c3228b23fa6109324e76b5faac57d4
Nathan N <nazzanuk@gmail.com>
2015-02-26 13:50:24 +0000

    shippable config update

--- build-server.sh
+++ build-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build --tag rambo-io .
-sudo docker run -p 5000:8082 rambo-io
+sudo docker run -p 5000:8082 --name="rambo-io" -d rambo-io
--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 $(boot2docker shellinit)
 docker build --tag rambo-io .
-docker run -p 5000:8082 rambo-io
+docker run -p 5000:8082 --name="rambo-io" -d rambo-io
