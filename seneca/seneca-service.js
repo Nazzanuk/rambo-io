60992bea6fc7c5084bd4c67a7ebd8f96ae7d6d86
Nathan N <nazzanuk@gmail.com>
2015-03-25 20:04:41 +0000

    updating deployment

--- seneca/seneca-service.js
+++ seneca/seneca-service.js
@@ -99,7 +99,7 @@ app.get('/projects', function (req, res) {
 });
 
 //Start
-var server = app.listen(80, function () {
+var server = app.listen(8082, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('App listening at http://%s:%s', host, port)
