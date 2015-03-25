e694d7b7e84b1e9dfd94659e73fe5a76bfb59332
Nathan N <nazzanuk@gmail.com>
2015-03-25 20:01:54 +0000

    updating deployment

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ssh -t -i deployment/andigital-website.pem ec2-user@52.16.53.51 bash -c "'
+ssh -t -i deployment/rambo-io-new.pem ec2-user@52.16.53.51 bash -c "'
 cd rambo-io
 git fetch
 git reset --hard origin/master
