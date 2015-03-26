b8469afb0f04ace1cb935d4670a1a7fae072d035
Nathan N <nazzanuk@gmail.com>
2015-03-26 10:45:39 +0000

    Start using .gitignore

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,9 +2,11 @@
 
 ssh -t -i deployment/rambo-io-new.pem ec2-user@52.11.61.74 bash -c "'
 cd rambo-io
-git fetch
-git reset --hard origin/master
-git pull
+sudo git stash save --keep-index
+sudo git stash drop
+sudo git fetch
+sudo git reset --hard origin/master
+sudo git pull
 sudo npm install --loglevel info
 bower install
 gulp
